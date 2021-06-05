cf976509fc4e36439925b28bdf89e7334c0b1762
Keikishim2 <Keikishim2@gmail.com>
2021-06-05 08:33:20 +0800

    Action

---
+++ survey-form-v2/src/models/model.js
@@ -0,0 +1,30 @@
+const connection    = require('../config/database.js');
+
+/*
+	Docu: Model Class
+		- req (http request object) - store all the query logs if the request object is passed
+		- force_log (true / false) - set to true to force the logging feature (Even for queries run in executeQuery(...))
+	used by: almost all models
+*/
+class Model {
+	constructor(req = undefined, force_log = false){
+		this.req = req;
+		this.force_log = force_log;
+	}
+	
+	
+
+	executeQuery(query) {
+		return new Promise((resolve, reject) => {
+			connection.query(query, function (err, result) {
+				if(err) {
+					reject(err);
+				}else{
+		        	resolve(result);
+		        }
+		    });
+		});		
+	}
+}
+
+module.exports = Model;
